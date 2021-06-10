6ec77ebe672b5ce0cfcdfe3e162bf56cd6303971
Kurnia Muhamad <omenkzz@gmail.com>
2021-06-10 19:17:49 +0700

    Fix sockjs issue

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -27,6 +27,7 @@ module.exports = merge(sharedConfig, {
   },
 
   devServer: {
+    socket: 'socket',
     clientLogLevel: 'none',
     compress: settings.dev_server.compress,
     quiet: settings.dev_server.quiet,
